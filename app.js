fc1659930efa416358644db29f74acbc988d2bb6
OsvaldoAc777 <84117103+OsvaldoAc777@users.noreply.github.com>
2021-05-18 18:14:16 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//requires packafe and set the port
+const express = require('express');
+const port = 3002;
+//para permitir el manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
+
+//Usar node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+//iniciar el servidor
+const server = app.listen(port, (error) =>{
+    if(error) return console.log('Error: ${error}');
+
+    console.log('El servidor escucha en el puerto ${server.address().port}');
+});
